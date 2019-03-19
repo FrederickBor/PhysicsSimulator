625947111f4423d524559a3aca10e167c5d82e0d
Juanjo <jplazacampillo@gmail.com>
2019-03-19 13:39:11 +0100

    Toma de contacto:
    	- Elimino mi rama previa y creo una nuevo a partir de la de la defensa
    de la práctica (frederick).
    
    	- Repaso el guión de la práctica 4 para retomar el hilo.
    	- Documento ciertos métodos ya implementados durante la práctica 4.
    
    	- Modifico el método addBody de PhysicsSimulator para que lance una
    excepción en caso de que haya dos bodies con el mismo id, según el guión
    de la pr4.
    	- Modifico los mensajes que lanzan las exceptions de PhysicsSimulator.
    	- Documento la clase PhysicsSimulator
    
    Me quedo por Controller para seguir repasando.

--- src/simulator/factories/Factory.java
+++ src/simulator/factories/Factory.java
@@ -5,7 +5,21 @@ import java.util.List;
 import org.json.JSONObject;
 
 public interface Factory<T> {
+	
+	/**
+	 * Este método será llamado para crear las instancias de la clase correspondiente - una instancia subtipo de T.
+	 * La estructura JSON que se pasa como parámetro al método createInstance incluye dos claves: type, que describe el tipo de objeto que se va a crear y; data, que es una estructura JSON que incluye toda la información necesaria para crear el objeto. 
+	 * 
+	 * @param info Estructura JSON que describe el objeto a crear.
+	 * @return Una instancia de la clase correspondiente - una instancia de un subtipo de T.
+	 * @throws IllegalArgumentException En caso de que info sea incorrecto.
+	 */
 	public T createInstance(JSONObject info) throws IllegalArgumentException;
 
+	/**
+	 * Devuelve una lista de objetos JSON, que son “plantillas” para structuras JSON válidas. Los objetos de esta lista se pueden pasar como parámetro al método createInstance. Esto es muy útil para saber cuáles son los valores válidos para una factoría concreta, sin saber mucho sobre la factoría en si misma. Por ejemplo, utilizaremos este método cuando mostremos al usuario los posibles valores de las leyes de la gravedad.
+	 * 
+	 * @return Una lista de objetos JSON, que son “plantillas” para structuras JSON válidas.
+	 */
 	public List<JSONObject> getInfo();
 }
--- src/simulator/model/PhysicsSimulator.java
+++ src/simulator/model/PhysicsSimulator.java
@@ -4,16 +4,17 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+
 public class PhysicsSimulator {
 	private double dt;
 	private GravityLaws gl;
 	private double timeLapsed;
 	private List<Body> bodies;
 	
-	public PhysicsSimulator(GravityLaws gl, double dt) throws IllegalArgumentException{
+	public PhysicsSimulator(GravityLaws gl, double dt) throws IllegalArgumentException {
 		
-		if (gl == null) throw new IllegalArgumentException();
-		if (dt <= 0) throw new IllegalArgumentException();
+		if (gl == null) throw new IllegalArgumentException("Tienes que poner una GravityLaw");
+		if (dt <= 0) throw new IllegalArgumentException("El Tiempo real por paso no puede ser menor o igual que 0");
 		
 		this.dt = dt;
 		this.gl = gl;
@@ -21,10 +22,25 @@ public class PhysicsSimulator {
 		timeLapsed = 0;
 	}
 	
-	public void addBody(Body b) {
+	/**
+	 * Añade el cuerpo b al simulador. El método comprueba que no existe ningún otro cuerpo con el mismo identificador. Si existiera, lanza una excepción del tipo IllegalArgumentException.
+	 * 
+	 * @param b Cuerpo que vamos a añadir a la simulación.
+	 * @throws IllegalArgumentException En caso de que haya dos cuerpos con el mismo id.
+	 */
+	public void addBody(Body b) throws IllegalArgumentException {
+		
+		for (Body body: bodies) {
+			if (b.id.equals(body.id))
+				throw new IllegalArgumentException("Parece que hay dos id iguales...");
+		}
+		
 		bodies.add(b);
 	}
 	
+	/**
+	 * Aplica un paso de simulación, i.e., primero llama al método apply de las leyes de la gravedad, después llama a move de cada cuerpo, donde dt es el tiempo real por paso, y finalmente incrementa el tiempo actual en dt segundos.
+	 */
 	public void advance() {
 		gl.apply(bodies);
 		for (Body body : bodies) {
@@ -33,6 +49,13 @@ public class PhysicsSimulator {
 		timeLapsed += dt;
 	}
 	
+	/**
+	 * Devuelve un String que representa un estado del simulador, utilizando el siguiente formato JSON:
+	 * 
+	 * { "time": T, "bodies": [json1, json2, ...] }
+	 * 
+	 * @return Un String que representa un estado del simulador.
+	 */
 	public String toString() {
 		StringBuilder s = new StringBuilder();
 		
