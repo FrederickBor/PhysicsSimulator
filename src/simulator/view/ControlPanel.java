bc451e21b52a9135df4dca14af45451b6648792d
jjopc <jjopc@jjopc-Gram>
2019-05-01 11:03:58 +0200

    PREVIO A HACER UNA NUEVA RAMA PARA LA PARTE OPCIONAL

--- src/simulator/view/ControlPanel.java
+++ src/simulator/view/ControlPanel.java
@@ -211,7 +211,8 @@ public class ControlPanel extends JPanel implements SimulatorObserver {
 							delayLocal = (Integer) ControlPanel.this.delay.getValue();
 							run_sim(stepsLocal, delayLocal);
 						} catch (Exception e) {
-							JOptionPane.showMessageDialog(ControlPanel.this, "Something went wrong loading some values (delay/steps/delta-time).",
+							JOptionPane.showMessageDialog(ControlPanel.this, 
+									"Something went wrong loading some values (delay/steps/delta-time).",
 									"Error Loading Parameters.", JOptionPane.ERROR_MESSAGE);
 						}
 
@@ -309,7 +310,8 @@ public class ControlPanel extends JPanel implements SimulatorObserver {
 					@Override
 					public void run() {
 						int messageType = JOptionPane.INFORMATION_MESSAGE;
-						JOptionPane.showMessageDialog(ControlPanel.this, "You have stopped the simulator.", "Simulation stopped", messageType);						
+						JOptionPane.showMessageDialog(ControlPanel.this, 
+								"You have stopped the simulator.", "Simulation stopped", messageType);						
 					}
 				});
 
